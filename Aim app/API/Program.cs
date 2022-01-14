7dd8beac1e92767ebeef64e5a4cf0a0abcd04cd0
Arsenii Merkulov <arsenii.vm@gmail.com>
2022-01-14 21:45:20 +0200

    fixed api assembly

--- Aim app/API/Program.cs
+++ Aim app/API/Program.cs
@@ -13,11 +13,7 @@ namespace API
     {
         public static void Main(string[] args)
         {
-            CreateHostBuilder(args).Build().Run();
+            
         }
-
-        public static IHostBuilder CreateHostBuilder(string[] args) =>
-            Host.CreateDefaultBuilder(args)
-                .ConfigureWebHostDefaults(webBuilder => { webBuilder.UseStartup<Startup>(); });
     }
 }
