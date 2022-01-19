d809ea3d7307e5ff196d9a92a7317d1b30ffe55e
Arsenii Merkulov <arsenii.vm@gmail.com>
2022-01-19 22:05:02 +0200

    added User.cs and Room.cs

---
+++ Aim app/Core/Room.cs
@@ -0,0 +1,22 @@
+﻿using System.Drawing;
+
+namespace Core
+{
+    public class Room
+    {
+        private string _filePath;
+        public int RoomId { get; set; }
+        public string RoomName { get; set; }
+        public Bitmap Photo { get; set; }
+
+        public string FilePath
+        {
+            get { return _filePath; }
+            set
+            {
+                _filePath = value;
+                Photo = new Bitmap(_filePath);
+            }
+        }
+    }
+}
---
+++ Aim app/Core/User.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Drawing;
+
+namespace Core
+{
+    public class User
+    {
+        private string _filePath;
+        public int UserId { get; set; }
+        public string UserName { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public Bitmap Photo { get; set; }
+
+        public string FilePath
+        {
+            get { return _filePath; }
+            set
+            {
+                _filePath = value;
+                Photo = new Bitmap(_filePath);
+            }
+        }
+    }
+}
