1810d15c4da168fbf6de0fdfbed235c559519ace
Arsenii Merkulov <arsenii.vm@gmail.com>
2022-01-19 22:13:38 +0200

    json worker updated

--- Aim app/DAL.Abstractions/Interfaces/IWorker.cs
+++ Aim app/DAL.Abstractions/Interfaces/IWorker.cs
@@ -2,6 +2,7 @@
 {
     public interface IWorker
     {
-        
+        public void SaveToFile<T>(string filePath, T obj);
+        public T LoadFromFile<T>(string filePath);
     }
 }
--- Aim app/DAL/Worker/JsonWorker.cs
+++ Aim app/DAL/Worker/JsonWorker.cs
@@ -1,9 +1,25 @@
-﻿using DAL.Abstractions.Interfaces;
+﻿using System.IO;
+using System.Text;
+using Newtonsoft.Json;
+using DAL.Abstractions.Interfaces;
 
 namespace DAL.Worker
 {
     public class JsonWorker : IWorker
     {
-        
+        public void SaveToFile<T>(string filePath, T obj)
+        {
+            var writeStream = File.OpenWrite(filePath);
+            var json = JsonConvert.SerializeObject(obj);
+            writeStream.Write(Encoding.UTF8.GetBytes(json));
+        }
+
+
+        public T LoadFromFile<T>(string filePath)
+        {
+            var writeStream = File.OpenRead(filePath);
+            var json = JsonConvert.DeserializeObject<T>(filePath);
+            return json;
+        }
     }
 }
