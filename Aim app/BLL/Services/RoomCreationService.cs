52150169b2a8c1225698b2b5ecb6e63f779d7215
Osigram <igor.b769@gmail.com>
2022-01-26 19:36:07 +0200

    Add room creation

---
+++ Aim app/BLL/Services/RoomCreationService.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Core;
+using DAL;
+using DAL.Abstractions.Interfaces;
+
+namespace BLL.Services
+{
+    public class RoomCreationService
+    {
+        private readonly IGenericRepository<Room> _roomGenericRepository;
+
+        public RoomCreationService(IGenericRepository<Room> roomRepository)
+        {
+            _roomGenericRepository = roomRepository;
+        }
+
+        public string CreateRoom(User user, string name, string description) //TODO: Photo implementation
+        {
+            var baseParticipantInfo = new ParticipantInfo()
+            {
+                RoleId = "1",
+                Notifications = true
+            };
+
+            var room = new Room()
+            {
+                RoomName = name,
+                RoomDescription = description,
+                Participants = new Dictionary<User, ParticipantInfo> { { user, baseParticipantInfo } }
+            };
+
+            _roomGenericRepository.CreateAsync(room).Wait();
+
+            return room.Id;
+        }
+
+        public void DeleteRoom(Room room, User user)
+        {
+            if (IsUserAdmin(room, user))
+            {
+                _roomGenericRepository.DeleteAsync(room).Wait();
+            }
+        }
+
+        public bool ChangeRoomSettings(Room room, User user, string name, string description) //TODO: photo implementation
+        {
+            if (IsUserAdmin(room, user))
+            {
+                if (!string.IsNullOrWhiteSpace(name))
+                {
+                    room.RoomName = name;
+                }
+                if (!string.IsNullOrWhiteSpace(description))
+                {
+                    room.RoomDescription = description;
+                }
+
+                _roomGenericRepository.UpdateAsync(room).Wait();
+
+                return true;
+            }
+
+            return false;
+        }
+
+        public bool IsUserAdmin(Room room, User user) //TODO: roles implementation
+        {
+            var isUserAdmin = room.Participants.Where(userPair => userPair.Value.RoleId == "1" && userPair.Key.Id == user.Id);
+
+            return isUserAdmin.Any();
+        }
+    }
+}
