16db7dbc301bc8cdd8aab7e6a400c318b2d8cdfa
Meeesha <81447820+D4RK21@users.noreply.github.com>
2022-01-30 18:56:19 +0300

    Delete last auth in registration

--- Aim app/BLL/Services/RegistrationService.cs
+++ Aim app/BLL/Services/RegistrationService.cs
@@ -13,15 +13,13 @@ namespace BLL.Services
         private readonly IPasswordService _passwordService;
         private readonly IGenericRepository<User> _genericRepository;
         private readonly ICurrentUser _currentUser;
-        private readonly IAuthorizationService _authorizationService;
 
         public RegistrationService(IPasswordService passwordService, IGenericRepository<User> genericRepository,
-            ICurrentUser currentUser, IAuthorizationService authorizationService)
+            ICurrentUser currentUser)
         {
             this._passwordService = passwordService;
             this._genericRepository = genericRepository;
             this._currentUser = currentUser;
-            this._authorizationService = authorizationService;
         }
 
         public async Task RegisterAsync(string userMail, string name, string surname, string nickName, string password,
@@ -38,10 +36,10 @@ namespace BLL.Services
             };
 
             await _passwordService.SetPassword(user, password);
+            
             await _genericRepository.CreateAsync(user);
             
             _currentUser.User = user;
-            await _authorizationService.UpdateLastAuth(user);
         }
     }
 }
