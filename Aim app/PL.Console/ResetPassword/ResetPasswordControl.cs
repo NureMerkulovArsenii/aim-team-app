eaa3ed62bed0e25368f7fdb1b7be1196817b5eee
Meeesha <81447820+D4RK21@users.noreply.github.com>
2022-01-30 14:34:41 +0300

    Add comparing codes in ResetPasswordControl

--- Aim app/PL.Console/ResetPassword/ResetPasswordControl.cs
+++ Aim app/PL.Console/ResetPassword/ResetPasswordControl.cs
@@ -24,52 +24,52 @@ namespace PL.Console.ResetPassword
         {
             try
             {
+                System.Console.Write("Enter your username/email: ");
+                var usernameOrEmail = System.Console.ReadLine()?.Trim();
+            
+                var userToReset = await _userService.GetUserByUserNameOrEmail(usernameOrEmail);
+            
+                while (userToReset == null)
+                {
+                    System.Console.WriteLine("Invalid username or email");
+                
+                    System.Console.Write("Enter your username/email: ");
+                    usernameOrEmail = System.Console.ReadLine()?.Trim();
+                
+                    userToReset = await _userService.GetUserByUserNameOrEmail(usernameOrEmail);
+                }
+            
+                var email = await _authorizationService.GetEmailByUsernameOrEmail(usernameOrEmail);
+                await _mailWorker.SendCodeByEmailAsync(email);
+            
+                System.Console.WriteLine($"We have sent message on your email ({email}) with code");
+                System.Console.Write("Enter code to reset your password: ");
+                var codeFromUser = System.Console.ReadLine()?.Trim();
 
+                while (_mailWorker.CompareCodes(codeFromUser))
+                {
+                    System.Console.Write("Wrong code! Enter code from message sent on your email or \"r\" to resend code: ");
+                    codeFromUser =  System.Console.ReadLine()?.Trim();
+
+                    if (codeFromUser == "r")
+                    {
+                        await _mailWorker.SendCodeByEmailAsync(email);
+                    }
+                }
+            
+                string password;
+                do
+                {
+                    System.Console.WriteLine("Enter new password: ");
+                    password = System.Console.ReadLine()?.Trim();
+                } while (string.IsNullOrWhiteSpace(password) || !_passwordService.HasPasswordCorrectFormat(email, password));
+
+                return await _passwordService.SetPassword(userToReset, password, true);
             }
             catch (Exception)
             {
+                return false;
             }
-            System.Console.Write("Enter your username/email: ");
-            var usernameOrEmail = System.Console.ReadLine()?.Trim();
-            
-            var userToReset = await _userService.GetUserByUserNameOrEmail(usernameOrEmail);
-            
-            while (userToReset == null)
-            {
-                System.Console.WriteLine("Invalid username or email");
-                
-                System.Console.Write("Enter your username/email: ");
-                usernameOrEmail = System.Console.ReadLine()?.Trim();
-                
-                userToReset = await _userService.GetUserByUserNameOrEmail(usernameOrEmail);
-            }
-            
-            var email = await _authorizationService.GetEmailByUsernameOrEmail(usernameOrEmail);
-            var code = await _mailWorker.SendCodeByEmailAsync(email);
-            
-            System.Console.WriteLine($"We have sent message on your email ({email}) with code");
-            System.Console.Write("Enter code to reset your password: ");
-            var codeFromUser = System.Console.ReadLine()?.Trim();
-
-            while (code != codeFromUser)
-            {
-                System.Console.Write("Wrong code! Enter code from message sent on your email or \"r\" to resend code: ");
-                codeFromUser =  System.Console.ReadLine()?.Trim();
-
-                if (codeFromUser == "r")
-                {
-                    await _mailWorker.SendCodeByEmailAsync(email, code);
-                }
-            }
-            
-            string password;
-            do
-            {
-                System.Console.WriteLine("Enter new password: ");
-                password = System.Console.ReadLine()?.Trim();
-            } while (string.IsNullOrWhiteSpace(password) || !_passwordService.HasPasswordCorrectFormat(email, password));
-
-            return await _passwordService.SetPassword(userToReset, password, true);
         }
     }
 }
